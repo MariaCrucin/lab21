7622b63cb3a18ea0c39e86a4cbd1684f67910b0f
Maria Crucin <crucin_maria@yahoo.com>
2019-05-11 11:31:21 +0300

    Initial commit

---
+++ lab-21-webapi/Models/TasksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab_21_webapi.Models
+{
+    // DbContext = Unit of Work
+    public class TasksDbContext: DbContext
+    {
+        public TasksDbContext(DbContextOptions<TasksDbContext> options) : base(options)
+        {
+        }
+
+        // DbSet = Repository
+        // DbSet = O tabela din baza de date
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ lab-21-webapi/Models/TasksDbSeeder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab_21_webapi.Models
+{
+    public class TasksDbSeeder
+    {
+        public static void Initialize(TasksDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any flowers.
+            if (context.Tasks.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Tasks.AddRange(
+                new Task
+                {
+                    Title = "Task 1",
+                    Description = "Test 1",
+                    DateAdded = DateTime.Now,
+                    Deadline = new DateTime(2019, 7, 10, 14, 30, 0),
+                    TaskImportance = (TaskImportance)1,
+                    TaskState = (TaskState)1
+                },
+                new Task
+                {
+                    Title = "Task 2",
+                    Description = "Test 2",
+                    DateAdded = DateTime.Now,
+                    Deadline = new DateTime(2019, 6, 25, 16, 0, 0),
+                    TaskImportance = (TaskImportance)2,
+                    TaskState = (TaskState)1
+                }
+            );
+            context.SaveChanges(); // commit transaction
+        }
+    }
+}
